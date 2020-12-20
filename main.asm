70396200c6e5680107ead25b34294918769667ae
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-12-20 12:51:24 +0200

    INC & DEC symbol

--- main.asm
+++ main.asm
@@ -1,22 +1,18 @@
 INCLUDE Irvine32.inc
 
 .DATA
-	
-	x DWORD 20005000h
-	y DWORD 60004000h
-	z DWORD 70003000h
-	finalVal Dword ?
+
+	myWord WORD 1000h
 
 .CODE
 main PROC
 	
-	; Hands on
+	inc myWord ; 1001
+	mov bx, myWord
+	dec bx ; 1000
 
-	MOV eax,x
-	ADD eax,y
-	SUB eax,z
-	mov finalVal, eax
-	
+	call dumpregs
+
 	exit
 main ENDP
 
