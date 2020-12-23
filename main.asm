e825dcfb2d2a76969feba108768852d78e671df8
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-12-23 18:46:54 +0200

    Q5

--- main.asm
+++ main.asm
@@ -1,21 +1,34 @@
 INCLUDE Irvine32.inc
 
 .DATA
-	
-	
 
 .CODE
 main PROC
 	
-	mov cx, 0B3A3H ; cx= B3A3H
-	movzx ebx,cx ; ebx = 0000B3A3H  ;cx = cl+ch so [cl = A3H] & [ch = B3H]
-	movzx edx,cl ; edx = 000000A3H
-	movzx cx,ch  ; cx  = 00B3H ; why cx only 4bit ecx => 8bit => cx -> 4bit
+	call readdec
 
+	mov ecx, eax
+	dec ecx
+	dec ecx
+	mov ebx , eax
+	Fac_loop:
+		mov esi, ecx
+		mov edx, ebx
+		sum_loop:
+			add ebx, edx
+		LOOP sum_loop
+		mov ecx, esi
+	LOOP Fac_loop
 
-	call dumpregs
+	mov eax, ebx
+	call writedec
+
 	exit
 main ENDP
+
+
+
+
 
 END main
 
