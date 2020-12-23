834704ae8687968efb3ef68d6e5a87207db102af
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-12-23 21:21:55 +0200

    Q4

--- main.asm
+++ main.asm
@@ -2,39 +2,62 @@ INCLUDE Irvine32.inc
 
 .DATA
 
+	Data DWORD 21 Dup(?) 
+	num DWORD ?
+	result DWORD 0
+
 .CODE
 main PROC
 	
-	call readdec
-	mov edx, eax ; a
-	call readdec
-	mov esi, eax ; r
-	call readdec
-	mov ecx, eax ; n
-
-	dec ecx
+	call readint
+	mov num, eax
+	add num, eax
+	mov edx , eax
 
 
-	mov eax,edx
-	call writedec
-		call crlf
+	mov ecx, num
 
-	Loop1:
-		mov ebx , ecx ; 5
-		mov ecx, esi
-		mov eax, edx
-
-		dec ecx
-
-		Loop2:
-			add eax,edx
-		LOOP Loop2
-		mov edx,eax
-		call writedec
-		call crlf
-		mov ecx , ebx
-	LOOP Loop1
+	mov edi , offset Data
 	
+	Loop1:
+		call readint
+		mov [edi], eax
+		add edi, 4
+	Loop Loop1
+
+	call readint
+
+	mov ecx , edx
+	mov edi , offset Data
+
+	Loop2:
+		; if( m >= arr[i] && m <= arr[i+1] ) =>  
+		; if( !m < arr[i] && !m > arr[i+1] ) => 
+		CMP eax , [edi]
+		JNB True1
+
+		add edi,8
+		JMP GoToLoopy
+		
+		True1: 
+			CMP eax , [edi+4]
+			JNA True2
+
+			add edi , 8
+
+			JMP GoToLoopy
+			True2:
+				inc result
+				add edi , 8
+
+
+	GoToLoopy: Loop Loop2
+
+	mov eax , result
+	call writedec
+
+
+
 
 	exit
 main ENDP
