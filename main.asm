753418e3836fb25b955caaf84e85e18999b27378
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-12-24 23:32:41 +0200

    Q2

--- main.asm
+++ main.asm
@@ -2,60 +2,53 @@ INCLUDE Irvine32.inc
 
 .DATA
 	
-	Data DWORD 21 Dup(?)
-	Data1 DWORD 21 Dup(?)
-	num DWORD ?
-	num1 DWORD ?
-	num2 DWORD ?
-	PO DWORD "["
-	PC DWORD "]"
-	COM DWORD ", "
+	str1Q2 byte"Please, Enter your first index: ",0
+	str2Q2 byte"Please, Enter your second index: ",0
+	str3Q2 byte"Please, Enter your word: ",0
+	str4Q2 byte 30 dup(?),0
+	str5Q2 byte "Your Swapped word: ",0
+	xQ2 dword ?
+	yQ2 dword ?
+	zzQ2 dword ?
 
 .CODE
 main PROC
 	
+	mov edx,offset str1Q2
+	mov esi, offset str4Q2
+	mov ecx,lengthof str4Q2
+	mov xQ2,ecx
+	call writestring 
 	call readdec
-	mov num,eax
+	mov yQ2,eax
+	;call crlf
 
-	mov edi , offset Data
-	mov ecx, num
-	Loop1:
-		call readdec
-		mov [edi], eax
-		add edi, 4
-	LOOP Loop1
-	mov edi , offset Data
-	mov edx, [edi]
-	mov num1,edx
-	add edi, 4
-	mov edx, [edi]
-	mov num2,edx
-	
-
-	mov edx, offset PO
+	mov edx,offset str2Q2
 	call writestring
-	
-	mov ecx, num
+	call readdec
+	mov zzQ2,eax
 
-	dec ecx
+	;call crlf
 
-	Loop2:
-		mov eax, [edi]
-		call writedec
-		mov edx,offset COM
-		call writestring
-		add edi, 4
-	LOOP Loop2
-
-	mov eax, num1
-	call writedec
-	mov edx,offset COM
+	mov edx,offset str3Q2
 	call writestring
-	mov eax, num2
-	call writedec
-	mov edx,offset PC
+	mov ecx,30
+	mov edx,offset str4Q2
+	call readstring	
+	mov esi,offset str4Q2
+	dec yQ2
+	dec zzQ2
+	mov edi,esi
+	add edi,yQ2
+	mov bl,[edi]
+	add esi,zzQ2
+	xchg bl,[esi]
+	mov [edi],bl
+	mov edx,offset str5Q2
 	call writestring
-
+	mov edx,offset str4Q2
+	call writestring
+	call crlf
 	exit
 main ENDP
 
