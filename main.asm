808ef1bd6c33c46cc20ffd20de867ed17f5f0166
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-12-24 23:56:39 +0200

    Q1

--- main.asm
+++ main.asm
@@ -2,71 +2,74 @@ INCLUDE Irvine32.inc
 
 .DATA
 	
-	s1Q3 byte " IS CORRECT",0
-	s2Q3 byte " IS ALMOST CORRECT",0
-	s3Q3 byte" IS WRONG",0
-	arr1Q3 byte 20 dup(0)
-	arr2Q3 byte 20 dup(0)
-	xxQ3 dword ?
-	YyQ3 dword ?
-	cntrQ3 dword 0
-
+	Data DWORD 21 Dup(?)
+	Data1 DWORD 21 Dup(?)
+	num DWORD ?
+	num1 DWORD ?
+	num2 DWORD ?
+	PO DWORD "["
+	PC DWORD "]"
+	COM DWORD " ,"
+	EnterQ1 BYTE "Enter number of Elements: ",0
+	Enter2Q1 BYTE "Enter your Elements: ",0
+	Enter3Q1 BYTE "Your Rotated Elements are: ",0
 .CODE
 main PROC
+
+	mov edx,offset EnterQ1
+	call writestring
 	
-	mov edx,offset arr1Q3
-	mov cntrQ3,0
-	mov ecx, lengthof arr1Q3
-	call readstring
-	mov xxQ3,eax
-	mov edx,offset arr2Q3
-	mov ecx,lengthof arr2Q3
-	call readstring
-	mov YyQ3,eax 
-	mov esi,offset arr1Q3
-	mov edi ,offset arr2Q3
-	mov edx,offset arr2Q3
+	call readdec
+	mov num,eax
+
+	mov edx,offset Enter2Q1
 	call writestring
-	mov eax,xxQ3
-	cmp eax,YyQ3
-	jae grtQ3 
 
-	mov ecx,YyQ3
-	JMP L1Q3
-	grtQ3:
-	mov ecx,xxQ3
+	mov edi , offset Data
+	mov ecx, num
+	Loop1:
+		call readdec
+		mov [edi], eax
+		add edi, 4
+	LOOP Loop1
 
-	l1Q3:
-
-	mov ebx,0
-	mov bl,[esi]
-	cmp bl,[edi]
-	je l2Q3
-	inc cntrQ3
-	l2Q3:
-	add esi,1
-	add edi,1
-	loop l1Q3
-	cmp cntrQ3,2
-
-	jbe corQ3
-	mov edx, offset s3Q3
+	mov edx,offset Enter3Q1
 	call writestring
-	jmp endingQ3
 
-	corQ3:
-	cmp cntrQ3,0
-	je correctQ3
-	mov edx,offset s2Q3
+	mov edi , offset Data
+	mov edx, [edi]
+	mov num1,edx
+	add edi, 4
+	mov edx, [edi]
+	mov num2,edx
+	
+
+	mov edx, offset PO
 	call writestring
-	jmp endingQ3
-	correctQ3:
-	mov edx,offset s1Q3
-	call Writestring
+	
+	mov ecx, num
 
+	dec ecx
+	dec ecx
+	add edi, 4
 
-	endingQ3:
-	call crlf
+	Loop2:
+		mov eax, [edi]
+		call writedec
+		mov edx,offset COM
+		call writestring
+		add edi, 4
+	LOOP Loop2
+
+	mov eax, num1
+	call writedec
+	mov edx,offset COM
+	call writestring
+	mov eax, num2
+	call writedec
+	mov edx,offset PC
+	call writestring
+
 	exit
 main ENDP
 
