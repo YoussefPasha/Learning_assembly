6ae13c07c9947e14d21dbd079401100b938689eb
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-12-25 00:27:24 +0200

    Q6

--- main.asm
+++ main.asm
@@ -1,35 +1,60 @@
 INCLUDE Irvine32.inc
 
 .DATA
-	EnterQ5 BYTE "Please Enter N: ",0
-	Enter2Q5 BYTE "The factorial is: ",0
+
+	EnterQ6 BYTE "Enter a: ",0
+	Enter2Q6 BYTE "Enter r: ",0
+	Enter3Q6 BYTE "Enter n: ",0
+	Enter4Q6 BYTE "The generated sequence:",0
 
 .CODE
 main PROC
+
+	mov edx,offset EnterQ6
+	call writestring
 	
-	mov edx,offset EnterQ5
+	call readdec
+	mov edi, eax ; a
+
+	mov edx,offset Enter2Q6
 	call writestring
 
 	call readdec
+	mov esi, eax ; r
 
-	mov ecx, eax
-	dec ecx
-	dec ecx
-	mov ebx , eax
-	Fac_loop:
-		mov esi, ecx
-		mov edx, ebx
-		sum_loop:
-			add ebx, edx
-		LOOP sum_loop
-		mov ecx, esi
-	LOOP Fac_loop
-
-	mov edx,offset Enter2Q5
+	mov edx,offset Enter3Q6
 	call writestring
 
-	mov eax, ebx
+	call readdec
+	mov ecx, eax ; n
+
+	dec ecx
+
+
+	mov edx,offset Enter4Q6
+	call writestring
+	call crlf
+
+	mov eax,edi
 	call writedec
+		call crlf
+
+	Loop1:
+		mov ebx , ecx ; 5
+		mov ecx, esi
+		mov eax, edi
+
+		dec ecx
+
+		Loop2:
+			add eax,edi
+		LOOP Loop2
+		mov edi,eax
+		call writedec
+		call crlf
+		mov ecx , ebx
+	LOOP Loop1
+	
 
 	exit
 main ENDP
