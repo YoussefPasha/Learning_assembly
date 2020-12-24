37ce445b3e0b25f0b6351aaf072ae8d7da020917
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-12-24 23:38:17 +0200

    Q3

--- main.asm
+++ main.asm
@@ -2,52 +2,70 @@ INCLUDE Irvine32.inc
 
 .DATA
 	
-	str1Q2 byte"Please, Enter your first index: ",0
-	str2Q2 byte"Please, Enter your second index: ",0
-	str3Q2 byte"Please, Enter your word: ",0
-	str4Q2 byte 30 dup(?),0
-	str5Q2 byte "Your Swapped word: ",0
-	xQ2 dword ?
-	yQ2 dword ?
-	zzQ2 dword ?
+	s1Q3 byte " IS CORRECT",0
+	s2Q3 byte " IS ALMOST CORRECT",0
+	s3Q3 byte" IS WRONG",0
+	arr1Q3 byte 20 dup(0)
+	arr2Q3 byte 20 dup(0)
+	xxQ3 dword ?
+	YyQ3 dword ?
+	cntrQ3 dword 0
 
 .CODE
 main PROC
 	
-	mov edx,offset str1Q2
-	mov esi, offset str4Q2
-	mov ecx,lengthof str4Q2
-	mov xQ2,ecx
-	call writestring 
-	call readdec
-	mov yQ2,eax
-	;call crlf
+	mov edx,offset arr1Q3
+	mov cntrQ3,0
+	mov ecx, lengthof arr1Q3
+	call readstring
+	mov xxQ3,eax
+	mov edx,offset arr2Q3
+	mov ecx,lengthof arr2Q3
+	call readstring
+	mov YyQ3,eax 
+	mov esi,offset arr1Q3
+	mov edi ,offset arr2Q3
+	mov edx,offset arr2Q3
+	call writestring
+	mov eax,xxQ3
+	cmp eax,YyQ3
+	jae grtQ3 
 
-	mov edx,offset str2Q2
-	call writestring
-	call readdec
-	mov zzQ2,eax
+	mov ecx,YyQ3
+	JMP L1Q3
+	grtQ3:
+	mov ecx,xxQ3
 
-	;call crlf
+	l1Q3:
 
-	mov edx,offset str3Q2
+	mov ebx,0
+	mov bl,[esi]
+	cmp bl,[edi]
+	je l2Q3
+	inc cntrQ3
+	l2Q3:
+	add esi,1
+	add edi,1
+	loop l1Q3
+	cmp cntrQ3,2
+
+	jbe corQ3
+	mov edx, offset s3Q3
 	call writestring
-	mov ecx,30
-	mov edx,offset str4Q2
-	call readstring	
-	mov esi,offset str4Q2
-	dec yQ2
-	dec zzQ2
-	mov edi,esi
-	add edi,yQ2
-	mov bl,[edi]
-	add esi,zzQ2
-	xchg bl,[esi]
-	mov [edi],bl
-	mov edx,offset str5Q2
+	jmp endingQ3
+
+	corQ3:
+	cmp cntrQ3,0
+	je correctQ3
+	mov edx,offset s2Q3
 	call writestring
-	mov edx,offset str4Q2
-	call writestring
+	jmp endingQ3
+	correctQ3:
+	mov edx,offset s1Q3
+	call Writestring
+
+
+	endingQ3:
 	call crlf
 	exit
 main ENDP
