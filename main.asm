abbe7058b753e8fb0fd2ae2b64bbc5b8d9e6fdb1
YoussefPasha <jojoyossefbasha010@gmail.com>
2020-12-28 06:29:24 +0200

    Q1(b)

--- main.asm
+++ main.asm
@@ -1,101 +1,76 @@
 INCLUDE Irvine32.inc
 
 .DATA
-	Count BYTE 0
-	Arr1 BYTE 21 dup("0")  
-	Arr2 BYTE 21 dup("0") 
-	isWrong BYTE " IS WRONG",0
-	isCorrect BYTE " IS CORRECT",0
-	isAlmostCorrect BYTE " IS ALMOST CORRECT",0
+
+	exEax DWORD ?
+	space BYTE " ",0
+	negma BYTE "*",0
+	exEcx DWORD ?
 
 .CODE
 main PROC
 	
-	mov Count ,0
-	; Arr1
-	mov edx, offset Arr1
-	mov ecx, Lengthof Arr1
-	call readstring
-	mov ebx, eax
+	call readdec
+	mov ecx , eax
+	mov exEax, eax
+	mov exEcx, eax
+	inc ecx
+	mov ebx, ecx	; ebx saved ecx Val 
+	Loop1:			; (b) Loop1
+	sub ecx, eax
+	mov edi , ebx
+	dec edi 
+	mov ecx, edi
+	Loop2:			; (b) Loop2
+	mov edx, offset space
+	call writestring
+	LOOP Loop2		; (e) Loop2
+	sub eax, ebx
+	add eax,eax
+	inc eax
+	inc eax
+	inc eax
+	mov ecx, eax
+	Loop3:			; (b) Loop3
+	mov edx, offset negma
+	call writestring
+	LOOP Loop3		; (e) Loop3
+	call crlf
 
-	; Arr2
-	mov edx, offset Arr2
-	mov ecx, Lengthof Arr2
-	call readstring
-	mov edx, eax
+	dec ebx
+	mov ecx, ebx
+	mov eax, exEax
 
-	; if(ebx > edx) => if(!ebx <= edx) JNBE
-	; if(ebx < edx) => if(!ebx >= edx) JNAE
-	; if(ebx = edx) => JNE
+	LOOP Loop1		; (e) Loop 1
+	mov ecx, exEcx
+	mov ebx,ecx
+	dec ecx
+	Loop4:			; (b) Loop4
+	sub eax,ecx
+	inc eax
+	mov ecx,eax
+	Loop5:			; (b) Loop5
+	mov edx, offset space
+	call writestring
+	LOOP Loop5		; (e) Loop5
+	mov ecx,ebx
+	add ecx,ecx
+	dec ecx
+	dec ecx
+	dec ecx
+	Loop6:			; (b) Loop6
+	mov edx, offset negma
+	call writestring
+	LOOP Loop6		; (e) Loop6
+	call crlf
 
-	CMP ebx , edx
-	JE True3
-	Jb True2
-	Ja True1
+	dec ebx
+	mov ecx, ebx
+	mov eax, exEax
+	LOOP Loop4
 
-	True1: 
-	mov eax,0
-		mov eax, ebx ; eax = ebx edx < eax
-		JMP OutPut
-	True2:
-	mov eax ,0
-		mov eax, edx ; eax = edx ebx < edx 
-		JMP OutPut
-	True3:
-	mov eax,0
-		mov eax, edx
-		JMP OutPut
-	 
-	 ; ebx = 10 edx = 5  eax = 10  
 
-	 OutPut:
-		
-		mov ecx,eax
-		mov edi , offset Arr1
-		mov esi , offset Arr2
-		mov eax,0
-	    Loop1:
-			mov al, [edi]
-			mov bl, [esi]
-			CMP al,bl
-			JNE True4
-			inc edi
-			inc esi
-			JMP goToLoopy
-			True4:
-				inc Count
-				inc edi
-				inc esi
-			goToLoopy:	LOOP Loop1
 
-	 CMP Count , 2 
-		Ja True5
-		CMP Count, 0 
-
-			JE True6
-
-			mov edx,0
-				mov edx , offset Arr2
-				call writestring
-				mov edx , offset isAlmostCorrect
-				call writestring
-				JMP quit
-
-			True5:
-			mov edx,0
-				mov edx , offset Arr2
-				call writestring
-				mov edx , offset isWrong
-				call writestring
-				JMP quit
-			True6:
-			mov edx,0
-				mov edx , offset Arr2
-				call writestring
-				mov edx , offset isCorrect
-				call writestring
-				JMP quit
-	quit:
 	exit
 main ENDP
 END main
